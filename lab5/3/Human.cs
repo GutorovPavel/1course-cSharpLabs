3cc6b0731c7b3a76debb7a977ab8a275f43f1914
GutorovPavel <pavelgutorovgut@gmail.com>
2021-05-06 15:41:29 +0300

    lab5

---
+++ lab5/3/Human.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    enum Sex
+    {
+        Male,
+        Female,
+        Undefined
+    }
+    abstract class Human
+    {
+        public Sex sex { get; set; }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        private string name, secondName;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string SecondName
+        {
+            get { return secondName; }
+            set { secondName = value; }
+        }
+
+        private List<Human> groupmate = new List<Human>();
+        public Human this[int index]
+        {
+            get { return groupmate[index]; }
+            set { groupmate.Add(value); }
+        }
+
+        public virtual void Information()
+        {
+            Console.WriteLine($"Info about {Name} {SecondName}:");
+            Console.WriteLine($"Age: {Age} \nSex: {sex}");
+        }
+    };
+    class Student : Human
+    {
+        private string university;
+        public string University
+        {
+            get { return university; }
+            set { university = value; }
+        }
+        private int course;
+        public int Сourse
+        {
+            get { return course; }
+            set { course = value; }
+        }
+        public override void Information()
+        {
+            base.Information();
+            if (University == null)
+            {
+                Console.WriteLine("He is not a student(\n");
+            }
+            else
+            {
+                if (sex == Sex.Male || sex == Sex.Undefined)
+                {
+                    Console.WriteLine($"He is a student of {University}");
+                }
+                else
+                {
+                    Console.WriteLine($"She is a student of {University}");
+                }
+            }
+        }
+    }
+    class Specialist : Student
+    {
+        private string speciality;
+        public string Speciality
+        {
+            get { return speciality; }
+            set { speciality = value; }
+        }
+        public override void Information()
+        {
+            base.Information();
+            if (University != null)
+            {
+                if (sex == Sex.Male || sex == Sex.Undefined)
+                {
+                    Console.WriteLine($"He is studying for an {Speciality}!\n");
+                }
+                else
+                {
+                    Console.WriteLine($"She is studying for an {Speciality}!\n");
+                }
+            }
+        }
+    }
+}
---
+++ lab5/3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Specialist bob = new Specialist();
+
+            bob.sex = Sex.Male;
+            bob.Age = 18;
+            bob.Name = "Bob";
+            bob.SecondName = "Marley";
+            bob.University = "BSUIR";
+            bob.Сourse = 1;
+            bob.Speciality = "CSPT";
+
+            Student lana = new Student();
+
+            lana.sex = Sex.Female;
+            lana.Age = 24;
+            lana.Name = "Lana";
+            lana.SecondName = "Rhoades";
+            lana.University = "BSU";
+
+            Specialist drake = new Specialist();
+
+            drake.sex = Sex.Male;
+            drake.Age = 25;
+            drake.Name = "Aubrey";
+            drake.SecondName = "Graham";
+
+            bob[0] = lana; 
+            bob[0] = drake;
+
+            bob.Information();
+            lana.Information();
+            Console.WriteLine();
+            drake.Information();
+
+            Console.ReadKey();
+        }
+    }
+}
+
